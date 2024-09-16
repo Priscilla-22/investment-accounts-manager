5e9af3caca33379bba22c9ab88e967202ce5dc71
Priscilla-22 <priscillamw123@gmail.com>
2024-09-16 15:41:29 +0300

    feat(models): add User, InvestmentAccounts, and Transaction models

--- api/models.py
+++ api/models.py
@@ -1,3 +1,20 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
+
 
 # Create your models here.
+class User(AbstractUser):
+    investment_accounts = models.ManyToManyField(
+        "InvestmentAccounts", related_name="user_investment_account"
+    )
+
+class InvestmentAccounts(models.Model):
+    name= models.CharField(max_length=100)
+    balance =models.DecimalField(max_digits=10, decimal_places=2)
+    user_investment_account = models.ManyToManyField(User, related_name='investment_accounts')
+
+
+class Transaction(models.Model):
+    investment_account = models.ForeignKey(InvestmentAccounts, on_delete=models.CASCADE)
+    amount = models.DecimalField(max_digits=10, decimal_places=2)
+    date = models.DateField()
