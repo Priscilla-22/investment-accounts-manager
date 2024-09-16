10045bad4ed4004751bd747d885a43d121a044e1
Priscilla-22 <priscillamw123@gmail.com>
2024-09-16 16:14:32 +0300

    feat: add serializers for investment accounts and transactions

---
+++ api/serializers.py
@@ -0,0 +1,19 @@
+from rest_framework import serializers
+from .models import InvestmentAccounts, Transaction
+
+
+class InvestmentAccountSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = InvestmentAccounts
+        fields = ["id", "name", "balance", "users"]
+
+
+class TransactionSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Transaction
+        fields = ["id", "investment_account", "amount", "date"]
+
+    def to_representation(self, instance):
+        representation = super().to_representation(instance)
+        representation["investment_account"] = instance.investment_account.name
+        return representation
