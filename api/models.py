252b05cee448c4bcfba9181adec17d60ab22725b
Priscilla-22 <priscillamw123@gmail.com>
2024-09-16 15:48:56 +0300

    refactor: omit related names

--- api/models.py
+++ api/models.py
@@ -5,13 +5,12 @@ from django.contrib.auth.models import AbstractUser
 # Create your models here.
 class User(AbstractUser):
     investment_accounts = models.ManyToManyField(
-        "InvestmentAccounts", related_name="user_investment_account"
-    )
+        "InvestmentAccounts")
 
 class InvestmentAccounts(models.Model):
     name= models.CharField(max_length=100)
     balance =models.DecimalField(max_digits=10, decimal_places=2)
-    user_investment_account = models.ManyToManyField(User, related_name='investment_accounts')
+    users = models.ManyToManyField(User)
 
 
 class Transaction(models.Model):
