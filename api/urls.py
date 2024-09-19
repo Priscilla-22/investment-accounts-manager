1af65dad2f6e09668fa84a9488d202552c9b0282
Priscilla-22 <priscillamw123@gmail.com>
2024-09-19 09:38:15 +0300

    feat(routers): set up DRF routers for InvestmentAccount and Transaction views

---
+++ api/urls.py
@@ -0,0 +1,12 @@
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+from .views import InvestmentAccountViewSet, TransactionViewSet, dashboard
+
+router = DefaultRouter()
+router.register(r"accounts", InvestmentAccountViewSet)
+router.register(r"transactions", TransactionViewSet)
+
+urlpatterns = [
+    # path('', dashboard, name='dashboard'),  
+    path("", include(router.urls)),  
+]
