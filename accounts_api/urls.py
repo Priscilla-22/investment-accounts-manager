8676190d2a98b1c54c4e27469ffdf16deef29f22
Priscilla-22 <priscillamw123@gmail.com>
2024-09-21 14:20:29 +0300

    feat: add the AdminViewSet to router

--- accounts_api/urls.py
+++ accounts_api/urls.py
@@ -1,11 +1,12 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from .views import InvestmentAccountViewSet, TransactionViewSet, dashboard
+from .views import InvestmentAccountViewSet, TransactionViewSet, dashboard, AdminViewSet
 
 
 router = DefaultRouter()
 router.register(r"accounts", InvestmentAccountViewSet)
 router.register(r"transactions", TransactionViewSet)
+router.register(r"admin", AdminViewSet, basename="admin")  
 
 urlpatterns = [
     # path('', dashboard, name='dashboard'),
